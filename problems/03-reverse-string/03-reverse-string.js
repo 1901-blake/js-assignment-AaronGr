30a876cf4ecfad80f173a866e57c90b466e9910a
Aaron Gravelle <agrav12825@gmail.com>
2019-01-15 13:00:38 -0500

    Finish Reverse String Code

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,11 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+    let someStrArray = someStr.split("");
+    let reverseStrArray = someStrArray.reverse();
+    let reverseStr = reverseStrArray.join("")
+
+    return reverseStr;
 }
+
+console.log(reverseStr("potato"));
