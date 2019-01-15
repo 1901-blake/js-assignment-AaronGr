91b96843be5d2d14ef7db93e1738311bc3340bf7
Aaron Gravelle <agrav12825@gmail.com>
2019-01-15 11:42:17 -0500

    Finish Palindrome Check Code

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,16 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+  let someStrArray = someStr.split("");
+  let reverseStrArray = someStrArray.reverse();
+  let reverseStr = reverseStrArray.join("")
+
+  if(someStr == reverseStr) {
+      return true;
+  } else {
+      return false;
+  }
 }
+
+console.log(isPalindrome('racecar'));
+console.log(isPalindrome('hello'));
