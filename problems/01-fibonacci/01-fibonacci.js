dceabe611eeaf67f7a1663deff5b7524e60ad561
Aaron Gravelle <agrav12825@gmail.com>
2019-01-15 10:35:52 -0500

    Write Fibonacci code

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,24 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
+    if( n == 0 || n == 1) {
+        return 0;
+    } else if( n == 2) {
+        return 1;
+    }
 
+    let num1 = 0;
+    let num2 = 1;
+    let nextNum = 0;
+    for(let i = 2; i < n; i++) {
+        nextNum = num1 + num2;
+        num1 = num2;
+        num2 = nextNum;
+    }
+
+    return nextNum;
+}
+
+for(let j = 1; j < 20; j++) {
+    console.log(fib(j)); 
 }
