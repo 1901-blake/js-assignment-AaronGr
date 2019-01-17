b1ab454e82db3ae3ecc181f2a3d86eb571f83fa9
Aaron Gravelle <agrav12825@gmail.com>
2019-01-17 09:11:15 -0500

    Finish Bubble Sort Code

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -4,8 +4,8 @@ Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
 function bubbleSort(numArray) {
     for(let i = 0; i < numArray.length; i++) {
-        for(let j = 1; j < numArray.length; j++) {
-            if(numArray[i] > numArray[j]) {
+        for(let j = 0; j < i; j++) {
+            if(numArray[i] < numArray[j]) {
                 let lowerNum = numArray[j];
                 numArray[j] = numArray[i];
                 numArray[i] = lowerNum;
@@ -17,4 +17,8 @@ function bubbleSort(numArray) {
 }
 
 let testArray = [3, 5, 1, 8, 20, 6, 4];
+let testArray2 = [1, 2, 3, 4, 5, 6 , 7];
+let testArray3 = testArray2.reverse();
 console.log(bubbleSort(testArray));
+console.log(bubbleSort(testArray2));
+console.log(bubbleSort(testArray3));
